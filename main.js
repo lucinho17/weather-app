112450d9750ad4ddf5e50b8618e8e6d4ee1b32b7
Luka Borović <124844612+lucinho17@users.noreply.github.com>
2025-11-11 21:40:08 +0100

    api-key-removal

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function fetchWeatherData() {
         return
     }
 
-    const url = `https://api.weatherstack.com/current?access_key=0bd619a588249dd9cb5622a1781426b6&query=${city}`;
+    const url = `https://api.weatherstack.com/current?access_key={YOUR_API_KEY}&query=${city}`;
     const options = {
 	method: 'GET'
     };
